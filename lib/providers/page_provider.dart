61718a12628b83592b5a2cfb2f106a84c991a40d
DeftoLabs <deftolabs@gmail.com>
2024-05-10 15:24:54 -0400

    route definition

--- lib/providers/page_provider.dart
+++ lib/providers/page_provider.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:universal_html/html.dart' as html;
+
+
 
 class PageProvider extends ChangeNotifier {
 
-  PageController scrollController = new PageController ();
+  PageController scrollController = PageController ();
+
+  final List<String> _pages = ['home', 'videoview', 'about', 'portafolio', 'contact'];
+
+  int _currentIndex = 0;
+
+  createScrollController (String routeName) {
+    scrollController = PageController(initialPage: getPageIndex(routeName));
+
+    html.document.title = _pages [ getPageIndex(routeName)];
+
+    scrollController.addListener(() {
+      final index = (scrollController.page ?? 0).round();
+
+      if( index != _currentIndex ){
+      html.window.history.pushState(null, 'none', '#/${ _pages[index] }' );
+      _currentIndex = index;
+
+      html.document.title = _pages[index];
+      }
+
+      
+    });
+
+  }
+
+  int getPageIndex (String routeName) {
+    return (!_pages.contains(routeName) )
+            ? 0 : _pages.indexOf(routeName);
+  }
 
   goTo (int index) {
+
+    // final routeName = _pages [index];
+    // html.window.history.pushState(null, 'none', '#/${ _pages[index] }' );
 
     scrollController.animateToPage(
       index, 
--- lib/router/router_handlers.dart
+++ lib/router/router_handlers.dart
@@ -1,13 +1,21 @@
 import 'package:fluro/fluro.dart';
+import 'package:flutter_web_danieldefaria/providers/page_provider.dart';
 
 import 'package:flutter_web_danieldefaria/ui/pages/home_page.dart';
+import 'package:provider/provider.dart';
 
 
 final homeHandler = Handler(
   handlerFunc: (context, parameters) {
 
     final page = parameters ['page']!.first;
+
+    
+
     if (page != '/') {
+      final pageProvider = Provider.of<PageProvider>(context!, listen: false);
+      pageProvider.createScrollController(page);
+
       return const  HomePage();
     }
   },);
